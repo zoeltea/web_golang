f5c9ccee9c31c640cc845469ffd1056c8d7f4435
zoeltea <zoeltea@gmail.com>
2025-04-29 18:01:18 +0700

    changes main

--- main.go
+++ main.go
@@ -1,27 +1,43 @@
-package main
+package test
 
 import (
+	"account-service/database"
+	"account-service/services/be"
+	"account-service/services/fe"
+	"flag"
 	"fmt"
+	"log"
 	"net/http"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hallo dunia manca negara!")
-}
-
 func main() {
+	// Load .env file
+	// err := godotenv.Load()
+	// if err != nil {
+	// 	log.Fatal("Error loading .env file")
+	// }
+
+	// Parse command line arguments
+	flag.Parse()
+
+	// Initialize database
+	db, err := database.InitDB()
+	if err != nil {
+		log.Fatalf("Failed to connect to database: %v", err)
+	}
+	defer db.Close()
+
+	// Set up routes BE
+	be.AccountRoutes(db)
+	fe.HomeRoutes()
+	fe.AccountRoutes()
+
+	// port := os.Getenv("SERVER_PORT")
 	port := "8080"
 	if port == "" {
 		port = "8080"
 	}
 
-	http.HandleFunc("/", handler)
-
-	fmt.Println("Server berjalan...")
-	fmt.Printf("Server mendengarkan pada port %s\n", port)
-
-	err := http.ListenAndServe(":"+port, nil)
-	if err != nil {
-		fmt.Println("Error:", err)
-	}
+	fmt.Printf("Server running on port %s\n", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
