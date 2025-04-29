5c6ed61e5acced30dc69d1b46e3a994d6f2faa8d
zoeltea <zoeltea@gmail.com>
2025-04-29 17:34:27 +0700

    changes main

--- go.mod
+++ go.mod
@@ -2,7 +2,4 @@ module account-service
 
 go 1.23.4
 
-require (
-	github.com/joho/godotenv v1.5.1
-	github.com/lib/pq v1.10.9
-)
+require github.com/lib/pq v1.10.9
--- main.go
+++ main.go
@@ -1,43 +1,12 @@
-package main
+package main // Setiap program Go harus dimulai dengan deklarasi package
 
-import (
-	"account-service/database"
-	"account-service/services/be"
-	"account-service/services/fe"
-	"flag"
-	"fmt"
-	"log"
-	"net/http"
-)
+import "fmt" // Import package "fmt" untuk input/output (seperti printf/println)
 
 func main() {
+	fmt.Println("hello duniaaa")
 	// Load .env file
 	// err := godotenv.Load()
 	// if err != nil {
 	// 	log.Fatal("Error loading .env file")
 	// }
-
-	// Parse command line arguments
-	flag.Parse()
-
-	// Initialize database
-	db, err := database.InitDB()
-	if err != nil {
-		log.Fatalf("Failed to connect to database: %v", err)
-	}
-	defer db.Close()
-
-	// Set up routes BE
-	be.AccountRoutes(db)
-	fe.HomeRoutes()
-	fe.AccountRoutes()
-
-	// port := os.Getenv("SERVER_PORT")
-	port := "8080"
-	if port == "" {
-		port = "8080"
-	}
-
-	fmt.Printf("Server running on port %s\n", port)
-	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
